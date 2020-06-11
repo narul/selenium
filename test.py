8ee9a1bceac07cf5fb31a452b0fd8800e8fe4868
Narul-python <naruln94@mail.ru>
2020-06-12 00:55:29 +0600

    Add files via upload
    Edit variable to name, changes in find_element_by_name and send_keys

--- test.py
+++ test.py
@@ -2,15 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-subject = 'Python'
+name = 'Narul'
 browser = webdriver.Chrome()
 site = "https://www.google.com/"
 browser.get(site)
-write = browser.find_element_by_name('q')
-write.send_keys(subject)
-enter = browser.find_element_by_name('q')
-enter.send_keys(Keys.RETURN)
-for i in range(1, 5):
+elem = browser.find_element_by_name('q')
+elem.send_keys(name)
+elem.send_keys(Keys.RETURN)
+for i in range(2, 7):
     selector = '#rso > div:nth-child({}) > div > div.r > a > h3'.format(i)
     link = browser.find_element_by_css_selector(selector)
     print(link.text)
